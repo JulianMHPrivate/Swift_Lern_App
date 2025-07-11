99df98ff610cf24d2536ca154cb15fc030412a84
Julian Meyer <julian@Julians-MacBook-Air.local>
2025-07-11 15:54:15 +0200

    Initial commit

---
+++ Swift_Lern_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift_Lern_App
+//
+//  Created by Julian Meyer on 11.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift_Lern_App/Swift_Lern_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Lern_AppApp.swift
+//  Swift_Lern_App
+//
+//  Created by Julian Meyer on 11.07.25.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Lern_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
